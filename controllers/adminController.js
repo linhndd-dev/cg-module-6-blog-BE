cd7436749140975315003483945e1f746b6841e3
nguyendoanduylinh <nguyen.doan.duy.linh@gmail.com>
2022-10-04 09:15:42 +0700

    Add admincontroller

---
+++ controllers/adminController.js
@@ -0,0 +1,8 @@
+const User = require("../models/User");
+const Post = require("../models/Post");
+
+const adminController = {
+  showAllUser: async (req, res) => {},
+};
+
+module.exports = adminController;
