b286ca2536b5a573ce937e38b6b17131f8fb19a8
nguyendoanduylinh <nguyen.doan.duy.linh@gmail.com>
2022-10-04 09:38:33 +0700

    add admin getallPosts

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const adminController = require("../controllers/adminController");
 
 router.get("/users", adminController.getAllUsers);
+router.get("/posts", adminController.getAllPosts);
 
 module.exports = router;
