253c5e963adfa8637fdf46b23c6617522ad4a039
EdwinDX <you@example.comletiendung502@gmail.com>
2022-10-07 03:39:50 +0700

    like done and protected route

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const verifyToken = require("../middlewares/auth");
+const {verifyTokenAdmin} = require("../middlewares/auth");
 
 const adminController = require("../controllers/adminController");
 
-router.get("/posts/search", adminController.searchPostsByTitle);
-router.get("/posts", adminController.getAllPosts);
-router.delete("/posts/:id", adminController.deletePostById);
-router.get("/users", adminController.getAllUsers);
-router.put("/users/status/:id", adminController.setUserInactiveById);
-router.get("/users/search", adminController.searchUsersByUsername);
-router.get("/posts/:id", adminController.getPostById);
+router.get("/posts/search",verifyTokenAdmin, adminController.searchPostsByTitle);
+router.get("/posts",verifyTokenAdmin, adminController.getAllPosts);
+router.delete("/posts/:id",verifyTokenAdmin, adminController.deletePostById);
+router.get("/users",verifyTokenAdmin, adminController.getAllUsers);
+router.put("/users/status/:id",verifyTokenAdmin, adminController.setUserInactiveById);
+router.get("/users/search",verifyTokenAdmin, adminController.searchUsersByUsername);
+router.get("/posts/:id",verifyTokenAdmin, adminController.getPostById);
 
 module.exports = router;
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const postController = require("../controllers/postController");
 const router = express.Router();
-const verifyToken = require("../middlewares/auth");
+const {verifyToken, optionallyVerifyToken} = require("../middlewares/auth");
 
 
 router.get("/search",verifyToken, postController.getPostsBySearch)
 
-router.get("/guest", postController.getAllPublicPosts);
+router.get("/guest",optionallyVerifyToken, postController.getAllPublicPosts);
 router.get("/guest/author/:id",postController.getPublicPostsByUserId)
 router.get("/", verifyToken, postController.getAllPostsByUserId);
 router.get("/:id", postController.getMyPostsById);
 router.post("/", verifyToken, postController.createMyPost);
 router.put("/:id", verifyToken, postController.editMyPost);
 router.delete("/:id", verifyToken, postController.deleteMyPost);
+
+router.post("/like/:id",verifyToken,postController.likePost)
+router.delete("/like/:id",verifyToken,postController.unlikePost);
+router.get("/liked/:id",optionallyVerifyToken,postController.getUserLikedPosts)
 
 module.exports = router;
